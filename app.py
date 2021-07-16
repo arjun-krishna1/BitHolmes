0f97d8459246781f7fa0032c5bf52c734fb86117
Joshua Dietrich <Jdietrich0603@gmail.com>
2021-07-16 16:00:33 -0400

    auto delete and qr maker

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import *
-import data
-import os
-
+import data, os
+import qrcodefunctions as qrfunc
 from rq import check_addr
 
 app = Flask(__name__)
+
+
 
 
 @app.route("/<address>", methods = ["GET"])
@@ -23,7 +24,13 @@ def index(address = None):
     return render_template("base.html", output_value = fraud_value)
 
 
-
+@app.route("/qr/<address>", methods = ["GET"])
+@app.route('/qr/', methods = ["GET"])
+def qr(address = None):
+    qrfunc.delete_old_files()
+    qr_hash = qrfunc.make_website_link_qr(address)
+    location = url_for('static', filename = qr_hash)
+    return render_template("qr.html", location = location)
 
 
 @app.context_processor
