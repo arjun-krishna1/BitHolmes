024f2febc6e4b185255c62bbb0a6168040ccbdcc
Joe-Joe-Joe-Joe <joedolina@gmail.com>
2021-07-16 13:21:53 -0400

    fixing my dumb function

--- rq/__init__.py
+++ rq/__init__.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def get_token():
     with open('new_secrets.txt', 'r') as f:
         t = f.read()
@@ -11,8 +12,9 @@ def check_addr(address):
     key = get_token()
     call = f"https://www.bitcoinabuse.com/api/reports/check?address={address}&api_token={key}"
     response_json = requests.api.get(call).json()
-    count = response_json["count"]
-    if count:
+    print(response_json)
+    count = response_json.get("count", None)
+    if count is not None:
         count = int(count)
         if count > 0:
             return 1  # 1 or more fraud reports on this address
