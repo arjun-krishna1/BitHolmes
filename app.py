8d7d5da6a4127b25694dcc6877a8ac6109f47099
Joshua Dietrich <Jdietrich0603@gmail.com>
2021-07-16 12:15:45 -0400

    basic flask outline

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
 from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == "static":
+        filename = values.get('filename', None)
+        if filename:
+            path = os.path.join(app.root_path, endpoint, filename)
+            values['q'] = int(os.stat(path).st_mtime)
+    return url_for(endpoint, **values)
+
+
+if __name__ == "__main__":
+    # app.run(host='localhost', debug=False, port=8000, threaded=True)
+    app.run(debug=True)
